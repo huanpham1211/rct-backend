412eb08298c7e2484a46aecbf39158fbd99f8436
Phạm Nguyên Huân <160609418+huanpham1211@users.noreply.github.com>
2025-04-19 17:44:20 +0700

    Update models.py

--- models.py
+++ models.py
@@ -11,4 +11,44 @@ class Users(db.Model):
     password = db.Column(db.String(200), nullable=False)
     role = db.Column(db.String(20), nullable=False)
 
+class Patient(db.Model):
+    __tablename__ = "patient"
+    id = db.Column(db.Integer, primary_key=True)
+    study_id = db.Column(db.Integer, nullable=True)
+    site_id = db.Column(db.Integer, nullable=True)
+    para = db.Column(db.String(4), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    dob = db.Column(db.Date, nullable=False)
+    sex = db.Column(db.String(10), nullable=False)
+    entered_by = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
+    timestamp_created = db.Column(db.DateTime, default=datetime.utcnow)
+    timestamp_updated = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+class Site(db.Model):
+    __tablename__ = "site"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.Text)
+    timestamp_created = db.Column(db.DateTime, default=datetime.utcnow)
+    timestamp_updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
+
+class Study(db.Model):
+    __tablename__ = 'study'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    protocol_number = db.Column(db.String(100))
+    irb_number = db.Column(db.String(100))
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    timestamp_created = db.Column(db.DateTime, default=datetime.utcnow)
+    timestamp_updated = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+class StudySite(db.Model):
+    __tablename__ = 'study_site'
+    id = db.Column(db.Integer, primary_key=True)
+    study_id = db.Column(db.Integer, db.ForeignKey('study.id'), nullable=False)
+    site_id = db.Column(db.Integer, db.ForeignKey('site.id'), nullable=False)
+    timestamp_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 # (include Patient, Site, Study, etc. if needed)
