1bf55a18c23c6b4ecef1387fda7dd918ca80f729
Phạm Nguyên Huân <160609418+huanpham1211@users.noreply.github.com>
2025-04-20 18:12:24 +0700

    Update users.py

--- routes/users.py
+++ routes/users.py
@@ -18,7 +18,7 @@ def get_users():
         {
             "id": user.id,
             "username": user.username,
-            "role": user.role
+            "role": user.role,
             "first_name": user.first_name,
             "last_name": user.last_name,
             "title": user.title
